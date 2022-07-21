4e4ccde774f9d93a58b6ac94d8835317b4b2e769
Jonathan Amiez <jonathan.amiez@gmail.com>
2022-07-21 12:31:57 +0200

    Configure vscode

--- hosts/neutrino/home.nix
+++ hosts/neutrino/home.nix
@@ -31,6 +31,25 @@
     };
   };
 
+  programs.vscode = {
+    enable = true;
+    package = pkgs.vscode;
+    extensions = with pkgs.vscode-extensions; [
+      arrterian.nix-env-selector
+      eamodio.gitlens
+      golang.go
+      jnoortheen.nix-ide
+      ms-vsliveshare.vsliveshare
+      vscodevim.vim
+    ];
+    userSettings = {
+      "editor.fontFamily" = "JetBrainsMono Nerd Font";
+      "editor.fontLigatures" = true;
+      "editor.fontSize" = 13;
+      "workbench.colorTheme" = "Solarized Dark";
+    };
+  };
+
   #programs.vim = {
   #  plugins = with pkgs.vimPlugins; [ vim-airline ];
   #};
