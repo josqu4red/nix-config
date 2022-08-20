111e8d10d5d95d19ba222564b49f3f1ad41b782a
Jonathan Amiez <jonathan.amiez@gmail.com>
2022-08-20 21:20:56 +0200

    Enable gpg and zsh mods on boson

--- hosts/boson/home.nix
+++ hosts/boson/home.nix
@@ -14,11 +14,13 @@ in {
   my.home = {
     alacritty.enable = true;
     firefox.enable = true;
+    gpg.enable = true;
     i3.enable = true;
     nix-tools.enable = true;
     pass.enable = true;
     polybar.enable = true;
     tmux.enable = true;
     vim.enable = true;
+    zsh.enable = true;
   };
 }
