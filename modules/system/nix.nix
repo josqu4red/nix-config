1adc293e8a8d7128f5fcbec2a41912aabb188578
Jonathan Amiez <jonathan.amiez@gmail.com>
2022-09-01 21:57:06 +0200

    Update nix settings

--- modules/system/nix.nix
+++ modules/system/nix.nix
@@ -4,9 +4,15 @@
 
   nix = {
     package = pkgs.nixFlakes;
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 10d";
+    };
     settings = {
       experimental-features = [ "nix-command" "flakes" ];
       cores = 0;
+      auto-optimise-store = true;
     };
   };
 }
