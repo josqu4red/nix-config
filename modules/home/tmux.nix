afdd1eaa57f56f7c24484035cf33b339126638ff
Jonathan Amiez <jonathan.amiez@gmail.com>
2022-09-19 18:33:14 +0200

    Set tmux nord theme

--- modules/home/tmux.nix
+++ modules/home/tmux.nix
@@ -10,13 +10,13 @@ in {
     programs.tmux = {
       enable = true;
       keyMode = "vi";
-      newSession = true;
       prefix = "M-a";
       terminal = "screen-256color";
       extraConfig = builtins.readFile ./tmux/tmux.conf;
       plugins = with pkgs.tmuxPlugins; [
         prefix-highlight
         vim-tmux-navigator
+        nord
       ];
     };
   };
