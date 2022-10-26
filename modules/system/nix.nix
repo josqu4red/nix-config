866fdb49b69c5f7f12440ddb59b074ba5db3da84
Jonathan Amiez <jonathan.amiez@gmail.com>
2022-10-26 23:40:11 +0200

    Increase delay to 30 days between GCs

--- modules/system/nix.nix
+++ modules/system/nix.nix
@@ -7,7 +7,7 @@
     gc = {
       automatic = true;
       dates = "weekly";
-      options = "--delete-older-than 10d";
+      options = "--delete-older-than 30d";
     };
     settings = {
       allowed-users = [ "@wheel" ];
