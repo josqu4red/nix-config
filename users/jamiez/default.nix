742a775898ee132e0f36a7581c52ab47f612166e
Jonathan Amiez <jonathan.amiez@gmail.com>
2022-11-26 15:41:49 +0100

    Join dialout group for arduino

--- users/jamiez/default.nix
+++ users/jamiez/default.nix
@@ -3,7 +3,7 @@
   users.users.jamiez = {
     isNormalUser = true;
     shell = pkgs.zsh;
-    extraGroups = [ "wheel" ];
+    extraGroups = [ "wheel" "dialout" ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID0iPZisp217gCwxTTizJ3HMbTIyRzYzAPDZA3kML2D2 jamiez@boson"
     ];
