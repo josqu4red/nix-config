c537d32573b5bd4e4209c519c958ec17b5ca5ce4
Jonathan Amiez <jonathan.amiez@gmail.com>
2023-02-25 15:04:19 +0100

    Import disko

--- flake.nix
+++ flake.nix
@@ -7,6 +7,10 @@
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs:
@@ -33,7 +37,7 @@
 
     systemModules = attrValues (import ./modules/options)
                     ++ attrValues (import ./modules/system)
-                    ++ [ ];
+                    ++ [ inputs.disko.nixosModules.disko ];
   in {
     inherit legacyPackages;
 
