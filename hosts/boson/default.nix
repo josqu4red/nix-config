024dccb5a129f8059b5e232c7069ed59ef800245
Jonathan Amiez <jonathan.amiez@gmail.com>
2023-03-01 18:44:34 +0100

    Enable aarch64 emulation with binfmt_misc

--- hosts/boson/default.nix
+++ hosts/boson/default.nix
@@ -5,6 +5,8 @@
     inputs.nixos-hardware.nixosModules.common-gpu-nvidia-nonprime
   ];
 
+  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+
   my.system.desktop.i3 = true;
   my.system.desktop.layout = "fr";
   services.udisks2.enable = true;
