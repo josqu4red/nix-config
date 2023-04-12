0bc22d67dd9f24c201073d1a58946fd26faa9b96
Jonathan Amiez <jonathan.amiez@gmail.com>
2023-04-12 11:32:37 +0200

    Bump flake deps

--- users/jamiez/default.nix
+++ users/jamiez/default.nix
@@ -1,5 +1,6 @@
 { config, ... }:
 {
+  programs.zsh.enable = config.my.options.userShell.pname == "zsh";
   users.users.jamiez = {
     isNormalUser = true;
     shell = config.my.options.userShell;
