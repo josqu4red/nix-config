b544b18573c5e94c3721a2aad36e4a0f5a80d805
Jonathan Amiez <jonathan.amiez@gmail.com>
2023-04-12 11:33:55 +0200

    Install libreoffice on quark

--- users/sev/default.nix
+++ users/sev/default.nix
@@ -3,6 +3,12 @@
   users.users.sev = {
     isNormalUser = true;
     extraGroups = [ "wheel" ];
-    packages = with pkgs; [ firefox-wayland ];
+    packages = with pkgs; [
+      firefox-wayland
+      libreoffice
+      hunspell
+      hunspellDicts.en_US
+      hunspellDicts.fr-moderne
+    ];
   };
 }
