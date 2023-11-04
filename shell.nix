7cd173ee0deeb6c21ae1c7094b4b14208a187412
Jonathan Amiez <jonathan.amiez@gmail.com>
2023-11-04 14:50:04 +0100

    Do not switch nixos immediately after build

--- shell.nix
+++ shell.nix
@@ -33,7 +33,7 @@ let
       ${pkgs.nixos-rebuild}/bin/nixos-rebuild build --flake .#$host --target-host $fqdn --use-remote-sudo || exit 1
       ${pkgs.openssh}/bin/ssh -t $fqdn -- nix-shell -p nvd --run \"nvd diff /nix/var/nix/profiles/system $result\"
       if apply; then
-        ${pkgs.nixos-rebuild}/bin/nixos-rebuild switch --flake .#$host --target-host $fqdn --use-remote-sudo || exit 1
+        ${pkgs.nixos-rebuild}/bin/nixos-rebuild boot --flake .#$host --target-host $fqdn --use-remote-sudo || exit 1
       fi
     '')
 
@@ -43,7 +43,7 @@ let
       ${pkgs.nvd}/bin/nvd diff /nix/var/nix/profiles/system result
       if apply; then
         # TODO: pkgs.sudo: /nix/store/yvf9z9p3ghlpixikxk02ad6l5lnl1krg-sudo-1.9.12p1/bin/sudo must be owned by uid 0 and have the setuid bit set
-        sudo ${pkgs.nixos-rebuild}/bin/nixos-rebuild switch --flake .
+        sudo ${pkgs.nixos-rebuild}/bin/nixos-rebuild boot --flake .
       fi
     '')
 
