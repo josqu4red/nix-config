df01915ac9ae570622abcfaaf343beb58c827f08
Jonathan Amiez <jonathan.amiez@gmail.com>
2024-05-23 21:53:08 +0200

    Migrate charm to systemd-networkd + DHCP

--- hosts/charm/default.nix
+++ hosts/charm/default.nix
@@ -1,9 +1,4 @@
-{ inputs, lib, pkgs, pkgsCross, ... }: let
-  defaultGw = "192.168.1.1";
-  ipAddress = { address = "192.168.1.240"; prefixLength = 24; };
-  nameservers = ["192.168.1.250"];
-  domain = "in.amiez.io";
-in {
+{ inputs, lib, pkgs, pkgsCross, ... }: {
   imports = [
     inputs.self.nixosProfiles.server
     inputs.disko.nixosModules.disko
@@ -47,12 +42,8 @@ in {
     supportedFilesystems = lib.mkForce [ "vfat" "fat32" "exfat" "ext4" ];
   };
 
-  networking = {
-    inherit domain nameservers;
-    dhcpcd.enable = false;
-    defaultGateway.address = defaultGw;
-    interfaces.end0.ipv4.addresses = [ ipAddress ];
-  };
+  networking.useNetworkd = true;
+  systemd.network.enable = true;
 
   # flash_erase /dev/mtd0 0 0  (from mtdutils)
   # dd if=uboot-spi.img of=/dev/mtdblock0 bs=4K
