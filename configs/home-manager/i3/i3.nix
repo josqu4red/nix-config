6df739f204ac11a5d66b051438e906c2dec3a497
Jonathan Amiez <jonathan.amiez@gmail.com>
2024-06-01 12:03:23 +0200

    Add bindings for i3 scratchpad

--- configs/home-manager/i3/i3.nix
+++ configs/home-manager/i3/i3.nix
@@ -79,11 +79,11 @@ in {
       "${mod}+1" = "workspace 1";
       "${mod}+2" = "workspace 2";
       "${mod}+3" = "workspace 3";
-      "${mod}+4" = "workspace 4";
-      "${mod}+Shift+exclam" = "move container to workspace 1; workspace 1";
-      "${mod}+Shift+at" = "move container to workspace 2; workspace 2";
-      "${mod}+Shift+numbersign" = "move container to workspace 3; workspace 3";
-      "${mod}+Shift+dollar" = "move container to workspace 4; workspace 4";
+      "${mod}+4" = "scratchpad show";
+      "${mod}+Shift+1" = "move container to workspace 1; workspace 1";
+      "${mod}+Shift+2" = "move container to workspace 2; workspace 2";
+      "${mod}+Shift+3" = "move container to workspace 3; workspace 3";
+      "${mod}+Shift+4" = "move scratchpad";
       # multimedia
       "XF86AudioRaiseVolume"  = "${pactl} set-sink-volume @DEFAULT_SINK@ +5%";
       "XF86AudioLowerVolume"  = "${pactl} set-sink-volume @DEFAULT_SINK@ -5%";
