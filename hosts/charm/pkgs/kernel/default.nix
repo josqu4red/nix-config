6a6cf243ba7e59607b35afd0d173af4f96405f69
Jonathan Amiez <jonathan.amiez@gmail.com>
2024-06-30 14:07:05 +0200

    Update charm's kernel

--- hosts/charm/pkgs/kernel/default.nix
+++ hosts/charm/pkgs/kernel/default.nix
@@ -1,12 +1,12 @@
 { fetchFromGitHub, buildLinux, ... }: (buildLinux {
-  version = "6.9.0-rc6";
-  modDirVersion = "6.9.0-rc6";
-  extraMeta.branch = "6.9";
+  version = "6.10.0-rc5";
+  modDirVersion = "6.10.0-rc5";
+  extraMeta.branch = "6.10";
   # https://gitlab.collabora.com/hardware-enablement/rockchip-3588/linux
   src = fetchFromGitHub {
     owner = "K900";
     repo = "linux";
-    rev = "0c638f3f28789c6af7cbbbbaff2efd6dc268cc53";
-    hash = "sha256-qJXiCo8iL60zhZgSMLZwN8pdkEeqJ0CQHgRt/vjWmT4=";
+    rev = "0ed750b5f5937c83ea48664aa56ed3235ae6a305";
+    hash = "sha256-M59TlrSmI3tJsz50WFp8+ufS/I4GOk0XiIM2M9Tx4QY=";
   };
 })
