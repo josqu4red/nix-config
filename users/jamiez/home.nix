dc9fa5f7b333eae0f2d78d0da2c1df1fae7552a9
Jonathan Amiez <jonathan.amiez@gmail.com>
2024-08-27 23:20:41 +0200

    Add git-crypt to default packages

--- users/jamiez/home.nix
+++ users/jamiez/home.nix
@@ -13,7 +13,7 @@
     ];
   };
 
-  home.packages = with pkgs; [ logseq slack spotify tig ];
+  home.packages = with pkgs; [ logseq slack spotify tig git-crypt ];
   home.sessionPath = [ "$HOME/.local/bin" ];
 
   xdg.configFile."autostart/gnome-keyring-ssh.desktop".text = ''
