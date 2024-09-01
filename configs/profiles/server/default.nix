59d2c40e2874eec7c9fa703fea290091e82e3549
Jonathan Amiez <jonathan.amiez@gmail.com>
2024-09-01 19:48:47 +0200

    Add base shell aliases for servers

--- configs/profiles/server/default.nix
+++ configs/profiles/server/default.nix
@@ -8,4 +8,16 @@
   services.timesyncd.enable = true;
 
   nix.settings.trusted-users = [ "@wheel" ]; # TODO: figure out signing
+
+  programs.bash.shellAliases = {
+    l = "ls -lh";
+    la = "ls -Alh";
+    rr = "rm -rf";
+    md = "mkdir -p";
+    rd = "rmdir";
+    j = "sudo journalctl";
+    s = "sudo systemctl";
+    sudo = "sudo ";
+  };
+
 }
