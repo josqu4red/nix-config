070e457178dfca7e7b0a530055711e84ea1bd836
Jonathan Amiez <jonathan.amiez@gmail.com>
2024-09-20 22:24:50 +0200

    Remove deprecated gnome setting

--- modules/nixos/desktop/default.nix
+++ modules/nixos/desktop/default.nix
@@ -24,7 +24,6 @@ in {
         displayManager.gdm.enable = true;
         desktopManager.gnome.enable = true;
       };
-      services.gnome.gnome-online-miners.enable = lib.mkForce false;
       services.gnome.evolution-data-server.enable = lib.mkForce false;
       environment.systemPackages = [ pkgs.gnome-tweaks ];
     })
