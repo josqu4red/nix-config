c91dc18323e7d3b410eb4804c0135f340be2d108
Jonathan Amiez <jonathan.amiez@gmail.com>
2024-09-25 19:03:29 +0200

    Remove deprecated flameshot setting

--- modules/home-manager/i3/flameshot.nix
+++ modules/home-manager/i3/flameshot.nix
@@ -3,7 +3,6 @@
   settings = {
     General = {
       buttons = ''@Variant(\0\0\0\x7f\0\0\0\vQList<int>\0\0\0\0\t\0\0\0\0\0\0\0\x2\0\0\0\x6\0\0\0\x12\0\0\0\xf\0\0\0\b\0\0\0\n\0\0\0\v\0\0\0\x17)'';
-      checkForUpdates = "false";
       contrastOpacity = "188";
       contrastUiColor = colors.grey;
       disabledTrayIcon = "true";
