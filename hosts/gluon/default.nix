15903bdbb6e7242c9584093ee04f7957ebbcf767
Jonathan Amiez <jonathan.amiez@gmail.com>
2024-12-08 15:53:57 +0100

    Add gluon x86-64 server

---
+++ hosts/gluon/default.nix
@@ -0,0 +1,21 @@
+{ inputs, hostFacts, ... }:
+{
+  imports = [
+    inputs.disko.nixosModules.disko
+    inputs.self.nixosProfiles.server
+  ];
+
+  disko.devices = import ./disk-config.nix;
+
+  boot = {
+    binfmt.emulatedSystems = [ "aarch64-linux" ];
+    loader.systemd-boot.enable = true;
+    loader.efi.canTouchEfiVariables = true;
+    initrd.availableKernelModules = [ "nvme" "xhci_pci" "sd_mod" ];
+    initrd.kernelModules = [ "dm-snapshot" "usb_storage" ];
+    kernelModules = [ "kvm-amd" ];
+    tmp.cleanOnBoot = true;
+  };
+
+  nxmods.impermanence.enable = true;
+}
