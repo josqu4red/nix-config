cc992bbeeb2263b665105f2e224f7970d9e8efda
Jonathan Amiez <jonathan.amiez@gmail.com>
2025-01-31 14:12:38 +0100

    Tune nixlive vm config

--- hosts/nixlive/default.nix
+++ hosts/nixlive/default.nix
@@ -1,7 +1,15 @@
-{ inputs, modulesPath, ... }:
-{
+{ inputs, lib, modulesPath, ... }: {
   imports = [
     (modulesPath + "/installer/cd-dvd/installation-cd-base.nix")
     inputs.self.nixosProfiles.base
   ];
+
+  virtualisation.vmVariant.virtualisation = {
+    # diskSize = 10240;
+    # memorySize = 4096;
+    qemu.networkingOptions = lib.mkForce [
+      # requires "allow br0" in /etc/qemu/bridge.conf
+      "-net nic,model=virtio,macaddr=02:de:ad:00:06:66 -net bridge,br=br0"
+    ];
+  };
 }
