8d0a560f3be86dd0d47e27248d8fdc6ec16a079b
Jonathan Amiez <jonathan.amiez@gmail.com>
2025-04-14 15:27:45 +0200

    Install kubectl-explore with kube tooling

--- modules/home-manager/kube/default.nix
+++ modules/home-manager/kube/default.nix
@@ -1,9 +1,9 @@
-{ inputs, pkgs, ... }:
+{ pkgs, ... }:
 let
   helm = with pkgs; wrapHelm kubernetes-helm { plugins = [ kubernetes-helmPlugins.helm-diff ]; };
 in {
   home.packages = with pkgs; [
-    k9s kind kubectl kubectx helm stern
+    k9s kind kubectl kubectl-explore kubectx helm stern
   ];
 
   programs.zsh.shellAliases = {
