be56482d33e2b7ac3bd99277ef3828bf3ec6316c
Jonathan Amiez <jonathan.amiez@gmail.com>
2025-08-25 10:17:19 +0200

    Add more kube tooling

--- modules/home-manager/kube/default.nix
+++ modules/home-manager/kube/default.nix
@@ -1,9 +1,9 @@
 { pkgs, ... }:
 let
-  helm = with pkgs; wrapHelm kubernetes-helm { plugins = [ kubernetes-helmPlugins.helm-diff ]; };
+  helm = with pkgs; wrapHelm kubernetes-helm { plugins = with kubernetes-helmPlugins; [ helm-diff helm-git helm-unittest ]; };
 in {
   home.packages = with pkgs; [
-    k9s kind kubectl kubectl-explore kubectx helm stern
+    k9s kind kubectl kubectl-explore kubectx helm chart-testing
   ];
 
   programs.zsh.shellAliases = {
