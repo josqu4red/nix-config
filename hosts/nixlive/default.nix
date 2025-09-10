a619a7a06942370c340a069a3d7cff73c828ad90
Jonathan Amiez <jonathan.amiez@gmail.com>
2025-09-10 23:36:08 +0200

    Update nixlive with TLS-ready HTTP server

--- hosts/nixlive/default.nix
+++ hosts/nixlive/default.nix
@@ -1,8 +1,7 @@
-{ inputs, lib, ... }: {
+{ inputs, lib, pkgs, secrets, ... }: {
   imports = [
     inputs.nixos-generators.nixosModules.all-formats
     inputs.self.nixosProfiles.base
-    ../gluon/paperless.nix
   ];
 
   virtualisation.vmVariant.virtualisation = {
@@ -12,5 +11,36 @@
       # requires "allow br0" in /etc/qemu/bridge.conf
       "-net nic,model=virtio,macaddr=02:de:ad:00:06:66 -net bridge,br=br0"
     ];
+  };
+
+  security.acme = {
+    acceptTerms = true;
+    defaults = {
+      email = secrets.acme-email;
+      server = "https://acme-staging-v02.api.letsencrypt.org/directory";
+      dnsProvider = "cloudflare";
+      dnsResolver = "1.1.1.1:53";
+      # https://dash.cloudflare.com/ca9a409fba4035b96ba999bb185c815e/api-tokens
+      # Zone:Read DNS:Edit
+      environmentFile = pkgs.writeText "cloudflare-token" ''
+        CF_DNS_API_TOKEN=
+      '';
+    };
+  };
+
+  networking.firewall.allowedTCPPorts = [80 443];
+  services.nginx = {
+    enable = true;
+    recommendedProxySettings = true;
+    recommendedTlsSettings = true;
+    virtualHosts."test.amiez.xyz" = {
+      enableACME = true;
+      acmeRoot = null;
+      forceSSL = true;
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:28981";
+        proxyWebsockets = true;
+      };
+    };
   };
 }
