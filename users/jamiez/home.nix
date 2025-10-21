f6d4a76afaeefb81c4d39f3d9d477fc4cfabd78c
Jonathan Amiez <jonathan.amiez@gmail.com>
2025-10-21 08:48:44 +0200

    Add GPG pubkey

--- users/jamiez/home.nix
+++ users/jamiez/home.nix
@@ -1,6 +1,11 @@
 { inputs, pkgs, ... }: {
   imports = with inputs.self.homeModules; [ base firefox git gpg kitty neovim pass tmux zsh ];
 
+  programs.gpg.publicKeys = [{
+    source = ./files/4BBCA7023906BA07.gpg.asc;
+    trust = 5;
+  }];
+
   nixpkgs = {
     config = {
       allowUnfree = true;
