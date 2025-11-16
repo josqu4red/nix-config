84b95603147f1a21e8bdfd6654953e4fab9014f9
Jonathan Amiez <jonathan.amiez@gmail.com>
2025-11-16 21:08:10 +0100

    Update nvim lsp config for 0.11

--- modules/home-manager/neovim/lua/plugins/nvim-lsp.lua
+++ modules/home-manager/neovim/lua/plugins/nvim-lsp.lua
@@ -2,29 +2,18 @@ return {
   "neovim/nvim-lspconfig",
   cmd = "LspInfo",
   event = {"BufReadPre", "BufNewFile"},
-  dependencies = { "hrsh7th/cmp-nvim-lsp" },
   init = function()
     vim.opt.signcolumn = "yes"
   end,
   config = function()
-    local lsp_defaults = require("lspconfig").util.default_config
-
-    lsp_defaults.capabilities = vim.tbl_deep_extend(
-      "force",
-      lsp_defaults.capabilities,
-      require("cmp_nvim_lsp").default_capabilities()
-    )
-
-    -- LspAttach is where you enable features that only work
-    -- if there is a language server active in the file
     vim.api.nvim_create_autocmd("LspAttach", {
-      desc = "LSP actions",
-      callback = function(event)
-        local opts = {buffer = event.buf}
+      group = vim.api.nvim_create_augroup('my.lsp', {}),
+      callback = function(args)
+        local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+        local opts = {buffer = args.buf}
 
         vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
         vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
-        vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
         vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
         vim.keymap.set("n", "gt", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
         vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
@@ -33,20 +22,17 @@ return {
         vim.keymap.set("n", "gF", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
         vim.keymap.set("n", "gA", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
 
-        -- if client.server_capabilities.document_formatting then
-        --   map.set("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
-        -- elseif client.server_capabilities.document_range_formatting then
-        --   map.set("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
-        -- end
+        if client:supports_method('textDocument/declaration') then
+          vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
+        end
+
+        if client:supports_method('textDocument/completion') then
+          vim.lsp.completion.enable(true, client.id, args.buf)
+        end
       end,
     })
 
-    local lspconfig = require("lspconfig")
-    local servers = {"clangd", "gopls", "jsonnet_ls", "pyright", "nil_ls", "solargraph"}
-    for _, lsp in ipairs(servers) do
-      lspconfig[lsp].setup({})
-    end
+    -- vim.lsp.config("*", {}) for overrides
+    vim.lsp.enable({"clangd", "gopls", "jsonnet_ls", "pyright", "nil_ls", "solargraph"})
   end
 }
-
-
